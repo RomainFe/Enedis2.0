18989c48f19fba731f3b7c6cfae72ba0b6880fed
RomainFe <50668176+RomainFe@users.noreply.github.com>
2019-05-26 20:27:55 +0200

    Add files via upload

--- LinkyService.js
+++ LinkyService.js
@@ -17,7 +17,7 @@ const setTimeoutPromise = util.promisify(setTimeout);
 
 async function myfunction() {
 	session = await linky.login('pierredenarie@gmail.com', 'rk6c%Mmf'); 																															// connection a l'espace client et récuperation d'une session pour récupérer les données de consommation
-  //conso  = await session.getHourlyData();																																													// Création de la session pour récuperer la consommation
+  conso  = await session.getHourlyData();																																														// Création de la session pour récuperer la consommation
 	consoDay  = await session.getDailyData();																																													// Création de la session pour récuperer de la consommation de l'habitation au jour
 	//consoMonth  = await session.getMonthlyData();																																										// Création de la session pour récuperer de la consommation de l'habitation au mois
 	//consoYear  = await session.getYearlyData();																																											// Création de la session pour récuperer de la consommation de l'habitation à l'année
@@ -28,17 +28,14 @@ async function start() {																																																						//
   return myfunction();																																																							// Renvoie d'une promesse qui sera résolu plus tard
 }
 
-function intervalFunc() {
+function intervalFuncHour() {
 	(async() => {
 		await start();
-		console.log("ici");
-		//request('http://'+hostname+'/devicestate/create/?token='+token+'&devicetype=4&value='+conso[48].value, { json: true } )				//Requête de récuperation de la consommation de l'habitation
-		request('http://'+hostname+'/devicestate/create/?token='+token+'&devicetype=5&value='+consoDay[30].value, { json: true } )			//Requête de récuperation de la consommation de l'habitation au jour
-		//request('http://'+hostname+'/devicestate/create/?token='+token+'&devicetype=6&value='+consoMonth[12].value, { json: true } )	//Requête de récuperation de la consommation de l'habitation au Mois
-		//request('http://'+hostname+'/devicestate/create/?token='+token+'&devicetype=6&value='+consoYear.value, { json: true } )				//Requête de récuperation de la consommation de l'habitation à l'année
-		.then(function(value){
-			i++;																																																													//Fonction pour parcourir les données de consommation(test)
-				if(30 == i ){
+		console.log(conso[47].value);
+		request('http://'+hostname+'/devicestate/create/?token='+token+'&devicetype=4&value='+conso[i].value, { json: true } )					//Requête de récuperation de la consommation de l'habitation à lheure
+		.then(function(value){																																																					//Promise : à chaque requ^ete on incrémenta la valeur i pour récupérer la valeur suivante
+			i++;
+				if(47 == i ){
 					(async() => { await start(); })();
 					i=0;
 				}
@@ -46,5 +43,27 @@ function intervalFunc() {
 	})();
 
 };
+function intervalFuncDay() {
+	(async() => {
+		await start();
+		request('http://'+hostname+'/devicestate/create/?token='+token+'&devicetype=5&value='+consoDay[30].value, { json: true } )			//Requête de récuperation de la consommation de l'habitation au jour
+	})();
 
-setInterval(intervalFunc,18000);																																																		// Intervalle entre les requetes
+};
+//function intervalFuncMonth() {
+//	(async() => {
+//		await start();
+//		request('http://'+hostname+'/devicestate/create/?token='+token+'&devicetype=6&value='+consoMonth[12].value, { json: true } )	//Requête de récuperation de la consommation de l'habitation au Mois
+//	})();
+//};
+//function intervalFuncYear() {
+//	(async() => {
+//		await start();
+//		request('http://'+hostname+'/devicestate/create/?token='+token+'&devicetype=6&value='+consoYear.value, { json: true } )				//Requête de récuperation de la consommation de l'habitation à l'année
+//	})();
+//};
+
+setInterval(intervalFuncHour,1800000);																																															// Intervalle de Lancement des requêtes pour la consommation a l'heure
+setInterval(intervalFuncHour,86400000);																																															// Intervalle de Lancement des requêtes pour la consommation au jour
+//setInterval(intervalFuncHour,2592000000);																																													// Intervalle de Lancement des requêtes pour la consommation au mois
+//setInterval(intervalFuncHour,31536000000);																																												// Intervalle de Lancement des requêtes pour la consommation a l'année
